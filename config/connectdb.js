8031afd3fa0e83046e6dd8156a430bf68aef39d6
DivyanshuJswl <ansh8jais@gmail.com>
2025-04-02 16:42:41 +0530

    hhk

--- config/connectdb.js
+++ config/connectdb.js
@@ -10,8 +10,6 @@ const connectDB = async () => {
 
   try {
     const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     isConnected = db.connections[0].readyState === 1;
