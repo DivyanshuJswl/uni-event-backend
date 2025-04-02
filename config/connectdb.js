5da9c51141d8a9deb006ebfdbc7fbe430c75c3e9
DivyanshuJswl <ansh8jais@gmail.com>
2025-04-02 17:03:57 +0530

    db

--- config/connectdb.js
+++ config/connectdb.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       return;
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://DEV:ram2122@bajajdb.tmzis.mongodb.net/?retryWrites=true&w=majority&appName=BajajDB", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
